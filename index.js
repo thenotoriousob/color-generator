69580486637edff0cea16d8ff77845aebe2c0c6e
thenotoriousob <32715357+thenotoriousob@users.noreply.github.com>
2024-08-14 19:28:11 +0100

    Make enhancements suggested in review
    On initial display load in the default colors
    Improved accessibility for keyboard users
    Removed templates and implemented event delegation
    Added a try/catch to the api call

--- index.js
+++ index.js
@@ -3,53 +3,84 @@ const colorModesEl = document.querySelector(".color-modes");
 const colorPalletteEl = document.querySelector(".color-pallete");
 const colorModes = ["monochrome","monochrome-dark","monochrome-light","analogic","complement","analogic-complement","triad","quad"];
 
-getColorBtn.addEventListener("click", callColorApi);
-
 async function callColorApi() {
 
-    // Need to remove the # for the api
-    const color = document.getElementById("color-input").value.substring(1);
-    const mode = colorModesEl.value;
+    try {
+        // Need to remove the # for the api
+        const color = document.getElementById("color-input").value.substring(1);
+        const mode = colorModesEl.value;
 
-    const res = await fetch(`https://www.thecolorapi.com/scheme?hex=${color}&format=json&mode=${mode}&count=5`);
-    const data = await res.json();
+        const res = await fetch(`https://www.thecolorapi.com/scheme?hex=${color}&format=json&mode=${mode}&count=5`);
 
-    renderColors(data.colors);
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+
+        const data = await res.json();
+
+        renderColors(data.colors);
+
+    }
+
+    catch(error) {
+        console.error("failed...");
+    }
 };
 
 function renderColors(colorArr) {
 
-    const templateEl = document.getElementById("color-template");
-  
     colorPalletteEl.innerHTML = "";
 
     colorArr.forEach(color => {
 
+        // This decontructs a value from the hex object within the color object
         const { hex: { value } } = color;
 
-        // Clone the template so we can add it to the dom
-        let clonedTemplate = templateEl.content.cloneNode(true);
+        const colorItemHTML = `
+        <div class="color-container">
+            <div class="color" id="${value}" data-color="${value}" style="background-color: ${value}" tabindex="0">
+                <span class="tooltiptext" id="my-tooltip-${value}">Copy to clipboard</span>
+            </div>
+            <div class="hex-label">${value}</div>
+        </div>
+        `;
 
-        clonedTemplate.querySelector(".color").id = value;
-        clonedTemplate.querySelector(".hex-label").textContent = value;
-        clonedTemplate.querySelector(".tooltiptext").id = `my-tooltip-${value}`;
+        colorPalletteEl.innerHTML += colorItemHTML
 
-        colorPalletteEl.appendChild(clonedTemplate);
-
-        document.getElementById(value).style.backgroundColor = value;
-
-        // Create a click event on every color item
-        document.getElementById(value).addEventListener("click", (e) => {
-            copyToClipboard(e.target.id);
-
-            document.getElementById(`my-tooltip-${value}`).textContent = "Copied";
-        });
-
-        // When the user moves off the color item then reset the text incase it changed
-        document.getElementById(value).addEventListener("mouseout", () => {
-            document.getElementById(`my-tooltip-${value}`).textContent = "Copy to clipboard";
-        });
     });
+
+    setUpEvenListeners();
+};
+
+function setUpEvenListeners() {
+
+    colorPalletteEl.addEventListener("click", (e) => {
+        if (e.target.dataset.color) {
+            copyToClipboard(e.target.dataset.color);
+        };
+    });
+
+    // When the user moves off the color item then reset the text incase it changed
+    colorPalletteEl.addEventListener("mouseout", (e) => {
+        resetTooltipText(e);
+    });
+
+    // When the user moves off the color item then reset the text incase it changed
+    colorPalletteEl.addEventListener("focusout", (e) => {
+        resetTooltipText(e);
+    });
+
+    colorPalletteEl.addEventListener("keydown", (e) => {
+        if (e.target.dataset.color && e.key === "Enter") {
+            copyToClipboard(e.target.dataset.color);
+        };
+    });
+}
+
+function resetTooltipText(e) {
+    if (e.target.dataset.color) {
+        document.getElementById(`my-tooltip-${e.target.dataset.color}`).textContent = "Copy to clipboard";
+    };
 };
 
 function populateColorModes() {
@@ -70,6 +101,11 @@ function copyToClipboard(copyText) {
 
     navigator.clipboard.writeText(copyText);
 
-}
+    document.getElementById(`my-tooltip-${copyText}`).textContent = "Copied";
+
+};
+
+getColorBtn.addEventListener("click", callColorApi);
 
 populateColorModes();
+callColorApi();
